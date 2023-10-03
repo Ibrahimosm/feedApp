10e20b5d40bb374193720508e910a0b2c17958b6
Ibrahimosm <ibrahimosman@hotmail.com>
2023-10-03 11:02:05 -0400

    signup method created in UserService that takes a user and register the user in the system

--- src/main/java/com/bptn/feedApp/service/UserService.java
+++ src/main/java/com/bptn/feedApp/service/UserService.java
@@ -3,8 +3,8 @@ import com.bptn.feedApp.jpa.User;
 import com.bptn.feedApp.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,5 +23,14 @@ public class UserService {
 
     public void createUser(User user) {
         this.userRepository.save(user);
+    }
+
+    public User signup(User user){
+        user.setUsername(user.getUsername().toLowerCase());
+        user.setEmailId(user.getEmailId().toLowerCase());
+        user.setEmailVerified(false);
+        user.setCreatedOn(Timestamp.from(Instant.now()));
+        this.userRepository.save(user);
+        return user;
     }
 }
