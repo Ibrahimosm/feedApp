b75d66a3a767cf2e912a5d7264baed868944f338
Ibrahimosm <ibrahimosman@hotmail.com>
2023-10-03 10:28:57 -0400

    Feed entity set up with the according attributes, methods and annotations,mapped to the Feed table in our database, and mapped to the FeedMetaData entity with a One-to_many relationship

---
+++ src/main/java/com/bptn/feedApp/jpa/FeedMetaData.java
@@ -0,0 +1,9 @@
+package com.bptn.feedApp.jpa;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "\"FeedMetaData\"")
+public class FeedMetaData {
+}
--- src/main/java/com/bptn/feedApp/jpa/Profile.java
+++ src/main/java/com/bptn/feedApp/jpa/Profile.java
@@ -1,4 +1,101 @@
 package com.bptn.feedApp.jpa;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.*;
+import java.io.Serializable;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 
-public class Profile {
+@Entity
+@Table(name = "\"Profile\"")
+public class Profile implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="\"profileId\"")
+    @JsonProperty(access = Access.WRITE_ONLY)
+    private Integer profileId;
+
+    private String bio;
+    private String city;
+
+    public Integer getProfileId() {
+        return profileId;
+    }
+
+    public void setProfileId(Integer profileId) {
+        this.profileId = profileId;
+    }
+
+    public String getBio() {
+        return bio;
+    }
+
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getHeadline() {
+        return headline;
+    }
+
+    public void setHeadline(String headline) {
+        this.headline = headline;
+    }
+
+    public String getPicture() {
+        return picture;
+    }
+
+    public void setPicture(String picture) {
+        this.picture = picture;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    private String country;
+    private String headline;
+    private String picture;
+
+    @OneToOne
+    @JsonIgnore
+    @JoinColumn(name="\"userId\"")
+    private User user;
+
+    @Override
+    public String toString() {
+        return "Profile [profileId=" + profileId + ", bio=" + bio + ", city=" + city + ", country=" + country + ", headline=" + headline + ", picture=" + picture + "]";
+    }
+    public Profile() {
+    }
+
 }
