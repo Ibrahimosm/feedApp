aee3adfb691779620c84c075cc08f2d0e7d1575e
Ibrahimosm <ibrahimosman@hotmail.com>
2023-10-03 10:47:33 -0400

    FeedMetaDataRepository interface created used to implement the backend logic for managing feed post metadata

---
+++ src/main/java/com/bptn/feedApp/repository/FeedMetaDataRepository.java
@@ -0,0 +1,7 @@
+package com.bptn.feedApp.repository;
+
+import com.bptn.feedApp.jpa.FeedMetaData;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FeedMetaDataRepository extends JpaRepository<FeedMetaData, Integer> {
+}
