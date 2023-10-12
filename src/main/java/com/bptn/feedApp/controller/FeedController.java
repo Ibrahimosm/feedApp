c5befceeac1c336e089aedc37bd5c1d17c9a0af4
Ibrahimosm <ibrahimosman@hotmail.com>
2023-10-12 13:25:19 -0400

    getUserFeeds method created in the FeedController that handles HTTP GET requests fpr a pageable response of feeds for the currently authenticated user within the FeedController

--- src/main/java/com/bptn/feedApp/controller/FeedController.java
+++ src/main/java/com/bptn/feedApp/controller/FeedController.java
@@ -1,4 +1,5 @@
 package com.bptn.feedApp.controller;
+import com.bptn.feedApp.domain.PageResponse;
 import com.bptn.feedApp.jpa.Feed;
 import com.bptn.feedApp.service.FeedService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,5 +30,12 @@ public class FeedController {
         logger.debug("Getting Feed, feedId: {}", feedId);
         return this.feedService.getFeedById(feedId);
 
+    }
+
+    @GetMapping("/user/{pageNum}/{pageSize}")
+    public PageResponse<Feed> getUserFeeds(@PathVariable int pageNum, @PathVariable int pageSize){
+        logger.debug("Getting User Feeds List, pageNum: {}, pageSize: {}", pageNum, pageSize);
+
+        return this.feedService.getUserFeeds(pageNum, pageSize);
     }
 }
