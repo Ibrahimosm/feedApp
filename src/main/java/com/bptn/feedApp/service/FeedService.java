5a9361449ac9cd47414142db34d8231c4136f859
Ibrahimosm <ibrahimosman@hotmail.com>
2023-10-14 01:47:23 -0400

    createFeedMEtaData method in the FeedService that creates a new FeedMetaData object with the provided metadata and saves it to the repository

--- src/main/java/com/bptn/feedApp/service/FeedService.java
+++ src/main/java/com/bptn/feedApp/service/FeedService.java
@@ -2,6 +2,9 @@ package com.bptn.feedApp.service;
 
 import com.bptn.feedApp.domain.PageResponse;
 import com.bptn.feedApp.exception.domain.FeedNotFoundException;
+import com.bptn.feedApp.exception.domain.LikeExistException;
+import com.bptn.feedApp.jpa.FeedMetaData;
+import com.bptn.feedApp.repository.FeedMetaDataRepository;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -13,6 +16,8 @@ import com.bptn.feedApp.repository.FeedRepository;
 import com.bptn.feedApp.repository.UserRepository;
 import java.sql.Timestamp;
 import java.time.Instant;
+import java.util.Optional;
+
 import org.springframework.security.core.context.SecurityContextHolder;
 import com.bptn.feedApp.exception.domain.UserNotFoundException;
 import com.bptn.feedApp.jpa.Feed;
@@ -28,6 +33,9 @@ public class FeedService {
 
     @Autowired
     FeedRepository feedRepository;
+
+    @Autowired
+    FeedMetaDataRepository feedMetaDataRepository;
 
     public Feed createFeed(Feed feed) {
 
@@ -66,6 +74,45 @@ public class FeedService {
         Page<Feed> paged = this.feedRepository.findByUserNot(user, PageRequest.of(pageNum, pageSize, Sort.by("feedId").descending()));
 
         return new PageResponse<Feed>(paged);
+    }
+
+    public FeedMetaData createFeedMetaData(int feedId, FeedMetaData meta) {
+
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = this.userRepository.findByUsername(username)
+                .orElseThrow(()-> new UserNotFoundException(String.format("Username doesn't exist, %s", username)));
+
+        Feed feed = this.feedRepository.findById(feedId)
+                .orElseThrow(()-> new FeedNotFoundException(String.format("Feed doesn't exist, %d", feedId)));
+
+        FeedMetaData newMeta = new FeedMetaData();
+
+        newMeta.setIsLike(false);
+        newMeta.setUser(user);
+        newMeta.setFeed(feed);
+        newMeta.setCreatedOn(Timestamp.from(Instant.now()));
+
+        if (Optional.ofNullable(meta.getIsLike()).isPresent()) {
+
+            newMeta.setIsLike( meta.getIsLike() );
+
+            if (meta.getIsLike()) {
+
+                feed.getFeedMetaData().stream()
+                        .filter(m -> m.getUser().getUsername().equals(username))
+                        .filter(m -> m.getIsLike().equals(true)).findAny()
+                        .ifPresent(m -> {throw new LikeExistException(String.format("Feed already liked, feedId: %d, username: %s", feedId, username));});
+
+                newMeta.setComment("");
+            }
+        }
+
+        if (!newMeta.getIsLike()) {
+            newMeta.setComment(meta.getComment());
+        }
+
+        return this.feedMetaDataRepository.save(newMeta);
     }
 
 }
